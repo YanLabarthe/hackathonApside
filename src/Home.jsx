31688d7443e8bd6b022cc4da185e625da8d3c406
Htms92 <saia.matteo992@gmail.com>
2022-06-29 15:56:22 +0200

    login

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import Login from "./pages/Login";
+
+
+const Home = () => {
+  return <div className="text-red-500 text-4xl">HOME IS WHERE HAYKUT IS
+ <Route path="/login" element={<Login />} />
+  </div>;
+  
+};
+
+export default Home;
--- src/components/Home.jsx
+++
@@ -1,5 +0,0 @@
-const Home = () => {
-  return <div className="text-red-500 text-4xl">HOME IS WHERE HAYKUT IS</div>;
-};
-
-export default Home;
