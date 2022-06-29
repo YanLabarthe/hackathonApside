fced82805541fabd3dc2c703b3a4544943a5bda8
Htms92 <saia.matteo992@gmail.com>
2022-06-29 16:22:35 +0200

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
-  // React States
+
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
 
-  // User Login info
+
   const database = [
     {
       username: "user1",
@@ -24,36 +24,36 @@ function Login() {
   };
 
   const handleSubmit = (event) => {
-    //Prevent page reload
+
     event.preventDefault();
 
     var { uname, pass } = document.forms[0];
 
-    // Find user login info
+
     const userData = database.find((user) => user.username === uname.value);
 
-    // Compare user info
+
     if (userData) {
       if (userData.password !== pass.value) {
-        // Invalid password
+
         setErrorMessages({ name: "pass", message: errors.pass });
       } else {
         setIsSubmitted(true);
       }
     } else {
-      // Username not found
+
       setErrorMessages({ name: "uname", message: errors.uname });
     }
   };
 
-  // Generate JSX code for error message
+
   const renderErrorMessage = (name) =>
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
     );
 
-  // JSX code for login form
-  const renderForm = (
+
+    const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
         <div className="input-container">
