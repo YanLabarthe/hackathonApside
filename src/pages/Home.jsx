945d65c37103a56e638b77899a345ebd52ec326c
Htms92 <saia.matteo992@gmail.com>
2022-06-29 16:12:16 +0200

    login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
-import Login from "./pages/Login";
 
 
 const Home = () => {
   return <div className="text-red-500 text-4xl">HOME IS WHERE HAYKUT IS
- <Route path="/login" element={<Login />} />
   </div>;
   
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
-
 import "./Login.css";
 
 function Login() {
