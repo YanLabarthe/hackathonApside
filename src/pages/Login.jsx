1771ee9b21e129521c4bbbcfc8753133177bdc41
Htms92 <saia.matteo992@gmail.com>
2022-06-30 10:28:40 +0200

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,14 +11,13 @@ const login = (e) => {
 	e.preventDefault();
 	console.log(email, password);
 
-	const userData = {
+const userData = {
 	email,
 	password,
 	};
 
 	localStorage.setItem('token-info', JSON.stringify(userData));
 	setIsLoggedin(true);
-	setName('');
 	setEmail('');
 	setPassword('');
 };
@@ -31,53 +30,62 @@ const logout = () => {
 return (
 	<>
 	<div style={{ textAlign: 'center' }}>
-		{!isLoggedin ? (
+	{!isLoggedin ? (
+
 		<>
-			<form action="" className="login">
-      <div className="input-container">
+	<form 
+	action="" 
+	className="login">
 
-			<input
-				type="email"
-				onChange={(e) => setEmail(e.target.value)}
-				value={email}
-				placeholder="Email" name="unmail"
-        required
-			/>
-      </div>
+	<div className="input-container">
+	<input
+	type="email"
+	onChange={(e) => setEmail(e.target.value)}
+	value={email}
+	placeholder="Email" 
+	name="unmail"
+	required
+	/>
+	</div>
 
-      <div className="input-container">
-			<input
-				type="password"
-				onChange={(e) => setPassword(e.target.value)}
-				value={password}
-				placeholder="Password"
-        name="pass"
-        required
-			/>
+	<div className="input-container">
+	<input
+	type="password"
+	onChange={(e) => setPassword(e.target.value)}
+	value={password}
+	placeholder="Password"
+	name="pass"
+	required
+	/>
+	</div>
 
-      </div>
+	<button 
+	class="button" 
+	type="submit" 
+	onClick={login}>
+	Login
+	</button>
 
-      <div className="button-container">
-			<button type="submit" onClick={login}>
-				Login
-			</button>
-      </div>
+	</form>
+	</>
+	) : (
+	<>
 
-			</form>
-		</>
-		) : (
-		<>
     <div className="login">
     <div className="login-form">
-      <div className="button-container">
-			<button onClickCapture={logout}>
-        Logout
-        </button>
-        </div>
-      </div>
-      </div>
+	<div className="button-container">
+
+	<button 
+	onClickCapture={logout}>
+	Logout
+	</button>
+
+	</div>
+	</div>
+	</div>
+	  
   	</>
-		)}
+	)}
 	</div>
 	</>
 );
