75b51510d347449172aade082ada4951a1fd73b3
Yan Labarthe <yan.labarthe@gmail.com>
2022-06-30 14:02:41 +0200

    !

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './components/Home'
+import ProjectFeed from "./pages/ProjectFeed";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/project-feed" element={<ProjectFeed />} />
       </Routes>
     </>
   );
