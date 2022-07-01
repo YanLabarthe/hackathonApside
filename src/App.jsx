6d20a0d217f1623d6bc1b679c8c874954f36df0c
Yan Labarthe <yan.labarthe@gmail.com>
2022-07-01 09:41:34 +0200

    !

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Home from './components/Home'
-<<<<<<< HEAD
-import Timeline from "./pages/Timeline";
-import WeekTimeline from "./pages/WeeklyTimeline"
-=======
-import Accueil from "./Accueil";
+import Accueil from "./pages/Accueil";
 import Timeline from "./components/page/Timeline";
 import WeekTimeline from "./components/page/WeeklyTimeline"
->>>>>>> b2619a53b0931681db9dabcc3e16839481775fca
 import ProjectFeed from "./pages/ProjectFeed";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Accueil" element={<Accueil/>}/>
+        <Route path="/" element={<Accueil/>}/>
         <Route path="/timeline" element={<Timeline />} />
         <Route path="/weekTime" element={<WeekTimeline />} />
         <Route path="/project-feed" element={<ProjectFeed />} />
--- src/components/Home.jsx
+++
@@ -1,8 +0,0 @@
-const Home = () => {
-  return <div className="text-red-500 text-4xl">HOME IS WHERE yannconnard IS</div>;
-
-  
-  return <div className="text-red-500 text-4xl">HOME IS WHERE HAYKUT IS</div>;
-};
-
-export default Home;
